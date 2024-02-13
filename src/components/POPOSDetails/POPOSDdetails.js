55fd5cc61c0cdfb476511a2f930cfab057ce46ff
el634dev <al7758066@gmail.com>
2024-02-13 14:45:41 -0500

    Added styling for details and lowered the border-radius

--- src/components/POPOSDetails/POPOSDdetails.js
+++ src/components/POPOSDetails/POPOSDdetails.js
@@ -1,10 +1,10 @@
 // Display details in a way where the user can re-vist or bookmark
-
-// React Libraries
 import React from "react";
 import { useParams } from 'react-router';
 // Importing data from JSON file
 import data from '../../sfpopos-data.json';
+// Styling
+import './POPOSDetails.css';
 
 function POPOSDetails(props) {
     const params = useParams();
@@ -14,18 +14,32 @@ function POPOSDetails(props) {
     const { images, title, desc, hours, features, geo } = data[id];
 
     return (
-        <div>
-            <div>
+        <div className="POPOSDetails">
+            <div className="POPOSDetails-image">
                 <img src={`${process.env.PUBLIC_URL}images/${images[0]}`} alt={title} />
             </div>
             {/**********/}
-            {/* POPOS Details */}
-            <div>
-                <h1>{ title }</h1>
-                <p>{ desc }</p>
-                <p>{ hours }</p>
-                <p>{ features }</p>
-                <p>{ geo.lat } { geo.lon }</p>
+            {/* POPOS Info */}
+            <div className="POPOSDetails-info">
+                <h1 className="POPOSDetails-title">
+                    { title }
+                </h1>
+                {/* POPOS Details */}
+                <p className="POPOSDetails-desc">
+                    { desc }
+                </p>
+                {/* POPOS Hours */}
+                <p className="POPOSDetails-hours">
+                    { hours }
+                </p>
+                {/* POPOS Features */}
+                <p className="POPOSDetails-features">
+                    { features }
+                </p>
+                {/* POPOS Geo */}
+                <p className="POPOSDetails-geo">
+                    { geo.lat } { geo.lon }
+                </p>
             </div>
         </div>
     );
