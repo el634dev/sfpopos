95f9a3f3cbf70592c197a1cda8d31f9783854928
el634dev <al7758066@gmail.com>
2024-04-10 16:05:02 -0400

    Fixed naming issues

--- src/components/Newsletter/Newsletter.js
+++ src/components/Newsletter/Newsletter.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import './Newsletter.css';
 
 function Newsletter() {
      return (
         <div>
-            <h2>Join Our Mailing List!</h2>
-            <small>Sign up to recieve the latest updates, new spaces, and more.</small>
-            <form>
-                <label>Email:</label>
-                <input type='text' />
+            <h1>Join Our Mailing List!</h1>
+            <h3>Sign up to recieve the latest updates, new spaces, and more.</h3>
+            <form className='newsletter'>
+                <label>Email</label>
+                <input type='text' placeholder='Enter your email'/><br />
                 {/* Name Input Field */}
-                <label>Name:</label>
-                <input type='text' />
-                {/* Submit Button */}
+                <label>Name</label>
+                <input type='text' placeholder='Enter your name'/>
+                {/* Submit Button */}<br />
                 <button type='submit'>Join</button>
             </form>
         </div>
