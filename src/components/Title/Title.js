c88680fe25973896839aa2993ae0431f7f59ecfa
el634dev <84924260+el634dev@users.noreply.github.com>
2024-04-16 14:14:13 -0400

    Update Title.js

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -18,7 +18,7 @@ function Title() {
                 </div>
                 {/************/}
                 <div className='Title-Nav'>
-                    <NavLink aria-label='link' className={({ isActive }) => isActive ? "nav-link-active" : "nav-link" } to="/">
+                    <NavLink aria-current='link' className={({ isActive }) => isActive ? "nav-link-active" : "nav-link" } to="/">
                         List
                     </NavLink>
                     {/********/}
